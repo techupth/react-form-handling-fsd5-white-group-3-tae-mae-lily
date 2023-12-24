38fcf87732681bb4b2f765d864e7c0b95302e673
Okubo Hazuki <okubohazuki@Okubos-MacBook-Air.local>
2023-12-24 22:26:21 +0900

    completed hh

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,26 @@
-function ProductForm() {
+import React, { useState } from "react";
+
+export default function ProductForm () {
+const [name, setName] = useState("");
+const [image, setImage] = useState("");
+const [price, setPrice] = useState("");
+const [description, setDescription] = useState("");
+
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(`name: ${name}, image: ${image}, price: ${price}, description: ${description}`); 
+  resetForm(); 
+};
+const resetForm = () => {
+  setName("");
+  setImage("");
+  setPrice("");
+  setDescription("");
+};
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +30,7 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(event) => {setName(event.target.value)}}
           />
         </label>
       </div>
@@ -22,7 +42,7 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(event) => {setImage(event.target.value)}}
           />
         </label>
       </div>
@@ -34,7 +54,7 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(event) => {setPrice(event.target.value)}}
           />
         </label>
       </div>
@@ -46,7 +66,7 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(event) => {setDescription(event.target.value)}}
             rows={4}
             cols={30}
           />
@@ -59,4 +79,4 @@ function ProductForm() {
   );
 }
 
-export default ProductForm;
+
